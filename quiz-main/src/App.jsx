da5d8c614dab2c59278b2e0666dab6aeb8fd3f40
Pradeesh <pradeesh2711.lm@gmail.com>
2024-07-17 21:11:44 +0530

    first commit

---
+++ quiz-main/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Quiz from './component/Quiz'
+
+function App() {
+  return (
+    <div className="App">
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
